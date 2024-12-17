894053e164a29d715130b10f449f3bcc34c8dac6
medlaq777 <m.laqsoumi@gmail.com>
2024-12-17 17:24:51 +0100

    Configuration de l'environnement : installation des dépendances et outils

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+  </header>
+  <main >
+    <p class="text-red-900">HELLO WORLD!!!</p>
+  </main>
+  <footer>
+  </footer>
+  <!-- <script src="scripts.js"></script> -->
+</body>
+</html>
